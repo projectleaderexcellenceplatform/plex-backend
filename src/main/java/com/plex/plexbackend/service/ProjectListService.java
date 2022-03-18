ee9d80f0db788f63a5b319b225e34b0612e213d0
Leon <Leonvandervleuten@gmail.com>
2022-03-18 20:06:49 +0100

    Removed api projectlist latest

--- src/main/java/com/plex/plexbackend/service/ProjectListService.java
+++ src/main/java/com/plex/plexbackend/service/ProjectListService.java
@@ -1,20 +1,13 @@
 package com.plex.plexbackend.service;
 
-import com.plex.plexbackend.domain.Project;
+
 import com.plex.plexbackend.domain.ProjectList;
 import com.plex.plexbackend.repository.ProjectListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 
 @Service
 public class ProjectListService {
@@ -32,14 +25,6 @@ public class ProjectListService {
 
   public void makeNewProjectList(ProjectList projectList) {
     projectListRepository.save(projectList);
-  }
-
-  public List<Project> findLatest(){
-    return new ArrayList<>(projectListRepository.findAll()
-        .stream()
-        .max(Comparator.comparing(ProjectList::getId))
-        .map(ProjectList::getProjects)
-        .orElse(Collections.emptySet()));
   }
 
 }
