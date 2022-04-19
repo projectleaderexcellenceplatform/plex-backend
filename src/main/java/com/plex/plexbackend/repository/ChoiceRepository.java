ea836d787be512b4475585bf7cf326ee3ae55ee1
Bart van de Leur <381729@student.fontys.nl>
2022-04-19 19:21:47 +0200

    Update ChoiceRepository Update ChoiceService

--- src/main/java/com/plex/plexbackend/repository/ChoiceRepository.java
+++ src/main/java/com/plex/plexbackend/repository/ChoiceRepository.java
@@ -1,9 +1,17 @@
 package com.plex.plexbackend.repository;
 
 import com.plex.plexbackend.domain.Choice;
+import com.plex.plexbackend.domain.Project;
+import com.plex.plexbackend.domain.ProjectList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChoiceRepository extends JpaRepository<Choice, Long> {
+
+    Long countChoicesByProjectAndAndProjectList(Project project, ProjectList projectList);
+
+    List <Choice> getAllByProjectAndProjectList(Project project, ProjectList projectList);
 }
